feb54b16b45c5c8bf503fc1e5be768913d243398
ck-sayan <105535261+ck-sayan@users.noreply.github.com>
2025-06-25 08:39:18 +0530

    learned variables and comments in dart

---
+++ comments.dart
@@ -0,0 +1,7 @@
+// In-line comment
+
+/* Block comment
+In this form comments can be
+multi-line comment */
+
+/// Documentation
---
+++ variables.dart
@@ -0,0 +1,9 @@
+import 'dart:core';
+
+void main() {
+
+  var firstName = "Sayan";
+  String lastName = "Chakraborty";
+
+  print (firstName + ' ' + lastName);
+}
